27631fb8f249b30a16996ce4aa819e0a80c4d384
Furkan Emin <femincanx@gmail.com>
2022-12-18 00:05:09 +0300

    feat: add project code

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-// index.js
-// where your node app starts
+const express = require('express');
+const cors = require('cors');
 
-// init project
-var express = require('express');
-var app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
+const app = express();
+app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get('/', (_, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get('/api', (_, res) => {
+  const currentDate = new Date();
+  res.json({ unix: Date.parse(currentDate), utc: currentDate.toUTCString() });
 });
 
+app.get('/api/:date', (req, res) => {
+  const dateParameter = req.params.date;
+  const dateString = isNaN(dateParameter)
+    ? dateParameter
+    : parseInt(dateParameter);
+  const dateObject = new Date(dateString);
 
+  dateObject.toString() === 'Invalid Date'
+    ? res.json({ error: 'Invalid Date' })
+    : res.json({
+        unix: Date.parse(dateObject),
+        utc: dateObject.toUTCString(),
+      });
+});
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
